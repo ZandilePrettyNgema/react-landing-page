48568a1f4bec43b4c86b4d2c4b5d54aa142cbcae
Zandile Pretty Ngema <zandilepretty01@gmail.com>
2024-04-02 04:26:35 +0200

    styled logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import logo from "./logo.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div className="App-logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="Navigation-bar"></div>
+      </header>
     </div>
   );
 }
