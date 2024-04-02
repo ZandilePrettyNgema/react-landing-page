4bbd3f382a4b8e5d302c56b6cc83d659130d598f
Zandile Pretty Ngema <zandilepretty01@gmail.com>
2024-04-02 03:55:28 +0200

    Removed all the unecessary files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
